5f0feba9a7429d6b1d2bc0da1f97acbe69370a49
IFISOLUTION\ndphong1 <phong.nguyendinh@emeal.nttdata.com>
2024-05-06 16:25:53 +0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_090629) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_06_091405) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
